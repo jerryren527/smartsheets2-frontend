ce601f1f0be1dc42fb1b1e69a08743eb76118de0
jerry <jerryren.5279@gmail.com>
2025-10-13 10:26:44 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [file1, setFile1] = useState(null);
+  const [file2, setFile2] = useState(null);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(file1);
+    console.log(file2);
+
+    // Send the files as multipart/form-data. This is how files are sent in request.FILES
+    const formData = new FormData();
+    if (file1) {
+      formData.append("file1", file1);
+    }
+    if (file2) {
+      formData.append("file2", file2);
+    }
+
+    const res = await axios.put("http://localhost:8000/app/join/", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(res);
+    console.log(res.data);
+
+    // Create a URL for the CSV blob
+    const url = window.URL.createObjectURL(new Blob([res.data]));
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "data.csv"); // filename
+    document.body.appendChild(link);
+    link.click();
+    link.remove(); // cleanup
+  };
+
+  return (
+    <>
+      <h1>Join two csv files</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="file" name="file1" id="file1" onChange={(e: any) => setFile1(e.target.files[0])} />
+        </div>
+        <div>
+          <input type="file" name="file2" id="file2" onChange={(e: any) => setFile2(e.target.files[0])} />
+        </div>
+        <button type="submit">Join!</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
